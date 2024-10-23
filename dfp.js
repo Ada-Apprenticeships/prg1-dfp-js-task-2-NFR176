ca610663ba810d2ed1162e6334918922a1f60b24
NFR176 <nevaeh.foster-ricket@ada.ac.uk>
2024-10-23 15:49:22 +0000

    addition of CamelCase v3

--- dfp.js
+++ dfp.js
@@ -1,11 +1,11 @@
 const fs = require('fs');//fs- the library being imported//
 
-function parseFile(indata, outdata, delimiter = ';') {// the function parsefile processes csv file//
-    if (!fs.existsSync(indata)) return -1; //if loop- checks if inputfile exists, if not return -1//
+function parseFile(inData, outData, delimiter = ';') {// the function parsefile processes csv file//
+    if (!fs.existsSync(inData)) return -1; //if loop- checks if inputfile exists, if not return -1//
     //note to self- the delimiter splits each line of the output and input content//
 
-    if (fs.existsSync(outdata)) fs.unlinkSync(outdata);// if output data exists-it will be deleted//
-    const lines = fs.readFileSync(indata, 'utf-8').split(/\n/).slice(1);
+    if (fs.existsSync(outData)) fs.unlinkSync(outData);// if output data exists-it will be deleted//
+    const lines = fs.readFileSync(inData, 'utf-8').split(/\n/).slice(1);
     let recordCount = 0;
 
     lines.forEach(line => {
@@ -14,7 +14,7 @@ function parseFile(indata, outdata, delimiter = ';') {// the function parsefile 
 
         const [review, sentiment] = trimmedLine.split(delimiter).map(item => item.trim()); //splits content into sentiment & review//
         const shortReview = review.substring(0, 20);//taking first 20 characters of 'review' to make 'shortreview'
-        fs.appendFileSync(outdata, `${sentiment}${delimiter}${shortReview}\n`, 'utf-8');//note-to-self- 'utf-8' is a form of character encoding//
+        fs.appendFileSync(outData, `${sentiment}${delimiter}${shortReview}\n`, 'utf-8');//note-to-self- 'utf-8' is a form of character encoding//
         recordCount++;
     });
 
